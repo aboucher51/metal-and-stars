ef4ea3d2a0dd0af85077e212bfaa7a49ad0a1719
Alex Boucher <aboucher51@gmail.com>
2025-08-25 22:49:47 -0400

    tiers not mandatory

--- prototypes/dependency-updates/planet-lib-tiers.lua
+++ prototypes/dependency-updates/planet-lib-tiers.lua
@@ -1,19 +1,23 @@
-local new_planet_tiers = {
-  nix = 3,
-  ringworld = 3,
-  shipyard = 2,
-}
+if mods["PlanetsLibTiers"] then
 
-for name, tier in pairs(new_planet_tiers) do
-  data.raw["mod-data"]["PlanetsLib-tierlist"].data.planet[name] = tier
-end
+  local new_planet_tiers = {
+    nix = 3,
+    ringworld = 3,
+    shipyard = 2,
+  }
 
-local new_space_locations = {
-  ["calidus-senestella-gate-senestella"] = 1.1,
-  ["calidus-senestella-gate-calidus"] = 1.2,
-  mirandus = 4,
-}
+  for name, tier in pairs(new_planet_tiers) do
+    data.raw["mod-data"]["PlanetsLib-tierlist"].data.planet[name] = tier
+  end
 
-for name, tier in pairs(new_space_locations) do
-  data.raw["mod-data"]["PlanetsLib-tierlist"].data["space-location"][name] = tier
+  local new_space_locations = {
+    ["calidus-senestella-gate-senestella"] = 1.1,
+    ["calidus-senestella-gate-calidus"] = 1.2,
+    mirandus = 4,
+  }
+
+  for name, tier in pairs(new_space_locations) do
+    data.raw["mod-data"]["PlanetsLib-tierlist"].data["space-location"][name] = tier
+  end
+
 end
